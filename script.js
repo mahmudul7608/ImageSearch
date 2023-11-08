6fda94d11b5cd435718ad564f8e0ad64745a5622
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-11-09 01:34:35 +0600

    complet API section and done this project

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const accessKey = 'NtrVlyQ61K4pFwyaf5tjugQCghpWVUnVHqAb1MVTRnM';
+const accessKey = "NtrVlyQ61K4pFwyaf5tjugQCghpWVUnVHqAb1MVTRnM";
 
-const searchForm = document.getElementById('search-form');
-const searchBox = document.getElementById('search-box');
-const searchResult = document.getElementById('search-result');
-const searchMoreBtn = document.getElementById('show-more-btn');
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const searchMoreBtn = document.getElementById("show-more-btn");
 
 let keyword = "";
 let page = 1;
 
 async function searchImages() {
   keyword = searchBox.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
   const response = await fetch(url);
   const data = await response.json();
+
+  if (page === 1) {
+    searchResult.innerHTML = "";
+  }
 
   const results = data.results;
 
@@ -28,11 +32,17 @@ async function searchImages() {
     imageLink.appendChild(image);
     searchResult.appendChild(imageLink);
   }
+  searchMoreBtn.style.display = "block";
 }
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   page = 1;
   searchResult.innerHTML = ""; // Clear the previous search results
+  searchImages();
+});
+
+searchMoreBtn.addEventListener("click", () => {
+  page++;
   searchImages();
 });

03fa7cab6e4bbbf8f85befdf5015683108da87fd
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2025-09-15 15:14:32 +0600

    Change the all UI design in Image search web and modify to working old project

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const searchMoreBtn = document.getElementById("show-more-btn");
+const resultsSection = document.getElementById("results-section");
 
 let keyword = "";
 let page = 1;
@@ -21,24 +22,29 @@ async function searchImages() {
 
   const results = data.results;
 
-  for (const result of results) {
+  results.forEach((result) => {
     const image = document.createElement("img");
     image.src = result.urls.small;
+    image.className = "w-full h-56 object-cover rounded-lg shadow";
 
     const imageLink = document.createElement("a");
     imageLink.href = result.links.html;
     imageLink.target = "_blank";
-
     imageLink.appendChild(image);
+
     searchResult.appendChild(imageLink);
+  });
+
+  if (results.length > 0) {
+    resultsSection.classList.remove("hidden");
+    resultsSection.classList.add("active");
+    searchMoreBtn.style.display = "block";
   }
-  searchMoreBtn.style.display = "block";
 }
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   page = 1;
-  searchResult.innerHTML = ""; // Clear the previous search results
   searchImages();
 });
 
